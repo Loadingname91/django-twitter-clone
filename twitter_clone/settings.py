d2ef49e80f389c14ee9b984f8bbcca18929d895d
Hitesh Balegar <gopinathhitesh@gmail.com>
2024-07-13 01:46:24 +0530

    addded static files collection

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -53,6 +53,7 @@ AUTH_USER_MODEL = 'user.CustomUser'
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
